cad37ff1de6ca5e872266d36f38a7547fdd1395c
Matt Spraggs <matthew.spraggs@gmail.com>
2016-05-26 15:07:31 -0400

    IO: Updated code to work with new file formats
    Unfortunately there's a bit of a backward compatability break. For those
    who use read_hdf5_to_numpy and wish to read the new data files, please
    use load_hdf5. For those who use the old file format, use
    load_hdf5_legacy, but try to update your file formats.

--- pylatan/io.py
+++ pylatan/io.py
@@ -1,49 +1,86 @@
 import os
+import warnings
 
 import h5py
 import numpy as np
 
 
-def read_hdf5_sample(filename):
-    """Read LatAnalyze3 HDF5 sample file"""
-    i = -1
-    if not os.path.exists(filename):
-        raise IOError("No such file: {}".format(filename))
-    f = h5py.File(filename)
-    outer_groupname = f.keys()[0]
-    outer_group = f[outer_groupname]
-    num_samples = outer_group.attrs['nSample'][0]
+warnings.simplefilter('always')
 
-    while i < num_samples:
-        key = "data_C" if i < 0 else 'data_S_{}'.format(i)
-        ret = outer_group[key].value
-        yield ret.reshape(ret.shape[::-1])
-        i += 1
+def _extract_samples(group, num_samples):
+    """Loop through samples in group and build the associated numpy array"""
 
-def read_hdf5_samples(filename):
-    """Read hdf5 samples from file, returning centre and samples"""
-    data = list(read_hdf5_sample(filename))
-    return data[0], data[1:]
-
-def read_hdf5_to_numpy(filename):
-    """Read hdf5 samples from file and return them as ndarray"""
-    if not os.path.exists(filename):
-        raise IOError("No such file: {}".format(filename))
-    f = h5py.File(filename)
-    outer_groupname = f.keys()[0]
-    outer_group = f[outer_groupname]
-    num_samples = outer_group.attrs['nSample'][0]
-
-    centre = outer_group["data_C"].value
-
+    centre = group["data_C"].value
     ret = np.zeros((num_samples + 1,) + centre.shape[::-1],
                    dtype=centre.dtype)
     ret[-1] = centre.reshape(centre.shape[::-1])
-    
+
     for i in range(num_samples):
         key = "data_C" if i == -1 else "data_S_{}".format(i)
-        data = outer_group[key].value
+        data = group[key].value
         ret[i] = data.reshape(data.shape[::-1])
 
     return ret
 
+def _common_hdf5_loading(filename):
+    """Common code for loading group and file from HDF5 file"""
+
+    if not os.path.exists(filename):
+        raise IOError("No such file: {}".format(filename))
+
+    f = h5py.File(filename)
+    outer_groupname = f.keys()[0]
+    outer_group = f[outer_groupname]
+    num_samples = outer_group.attrs['nSample'][0]
+    datatype = outer_group.attrs['type'][0]
+
+    return outer_group, num_samples, datatype
+
+
+def load_hdf5_legacy(filename):
+    """Read v3.0 hdf5 samples from file and return them as a numpy ndarray.
+
+    Args:
+      filename (str): Path to the hdf5 file to open.
+
+    Returns:
+      numpy.ndarray: First dimension is length num_samples + 1.
+
+      The first num_samples slices are the various bootstrap samples, the last
+      slice is the central value.
+    """
+
+    warnings.warn("LatAnalyze 3.0 file formats are no longer supported. "
+                  "Please convert your sample file if possible.",
+                  DeprecationWarning)
+
+    outer_group, num_samples, datatype = _common_hdf5_loading(filename)
+    return _extract_samples(outer_group, num_samples)
+
+
+def load_hdf5(filename):
+    """Load hdf5 data from file and return it as a numpy ndarray.
+
+    Args:
+      filename (str): Path to the hdf5 file to open.
+
+    Returns:
+      numpy.ndarray: The samples or other data stored in the given file.
+
+      For sample data, the first num_samples slices are the various bootstrap
+      samples, the last slice is the central value.
+    """
+    if not os.path.exists(filename):
+        raise IOError("No such file: {}".format(filename))
+
+    outer_group, num_samples, datatype = _common_hdf5_loading(filename)
+
+    if datatype not in [1, 2, 3]:
+        raise IOError("Unknown datatype information. Try loading with "
+                      "load_hdf5_legacy, or convert your input file.")
+
+    if datatype in [1, 3]:
+        return outer_group["data"][()]
+
+    elif datatype == 2:
+        return _extract_samples(outer_group, num_samples)
